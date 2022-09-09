d97167bfa1f4b92bb811d44324397120f74c275b
Jonathan East <jeast3113@gmail.com>
2022-09-08 22:12:25 -0400

    added functions for math operations and tested functions in the console

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log("Hello World");
+// -- create functions for math operations
+
+const add = (a, b) => {
+  return a + b;
+};
+
+const subtract = (a, b) => {
+  return a - b;
+};
+
+const multiply = (a, b) => {
+  return a * b;
+};
+
+const divide = (a, b) => {
+  return a / b;
+};
+
+//----------------//
+
